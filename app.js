a164cfd0bcba31cddb5d3a9123c9daac560e8d46
SaurabhKumarJ <149100538+SaurabhKumarJ@users.noreply.github.com>
2024-02-13 12:03:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 //APP CONFIG.
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/blogify")
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Mongo Connection open");
   })
